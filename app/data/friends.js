d3d25447e97cfd2c0535b91cadb447e421b7794a
Bobby H <rhoffman103@gmail.com>
2018-09-19 16:08:22 -0400

    basic friend finder

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,20 +1,69 @@
 var friends = [
     {
-        "name":"Ahmed",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name":"Doggo",
+        "photo": "https://i.pinimg.com/originals/60/9f/4b/609f4b1ba5f299ffdf54692dca3fdf39.jpg",
         "scores":[
             5,
-            1,
-            4,
-            4,
             5,
-            1,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
+        ]
+    },
+    {   
+        "name":"Fish Earl",
+        "photo": "https://images.pexels.com/photos/128756/pexels-photo-128756.jpeg?auto=compress&cs=tinysrgb&h=350",
+        "scores":[
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4
+        ]
+    },
+    {
+        "name":"Johny Camel",
+        "photo": "https://1.bp.blogspot.com/-XtKVlDBLLpU/TxllJl5sAKI/AAAAAAAAAzk/z2dpTcjG1xo/s1600/2-funny-laughing-animals-2-4-7-9-0-3-2-4-5-6-8-1-2-3-4.png",
+        "scores":[
             2,
-            5,
-            4,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2
+        ]
+    },
+    {
+        "name":"Mr. Bigglesworth",
+        "photo": "https://100-pics.net/images/answers/de/cats/cats_31310_296061.jpeg",
+        "scores":[
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
             1
         ]
-    }
+    },
+    
 ]
 
 module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,9 +9,50 @@ const api = function() {
     this.postNewFriend = function(app, friends) {
         app.post("/api/friends", function(req, res) {
             var newFriend = req.body;
-            newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
+            var friendsTotals = [];
+            var userTotal;
+            var friendMatch;
+
+            // Add total value of scores arrays
+            const getTotalScore = function(array) {
+                var sum = 0;
+                array.forEach(function(element) {
+                    sum += parseInt(element);
+                });
+                return sum;
+            };
+
+            // get array of the difference from (friends scores - user score)
+            const getFriendsListDifference = function() {
+                friends.forEach(function(element) {
+                    friendsTotals.push(Math.abs(getTotalScore(element.scores) - userTotal));
+                });
+            }
+
+            // Compare scores from friends with users score & return closest diference
+            const getFriendMatch = function() {
+                var closestMatch = friendsTotals[0];
+
+                friendsTotals.forEach(function(friendScore) {
+                    if (closestMatch > friendScore) {
+                        closestMatch = friendScore;
+                    }
+                });
+                friendMatch = friends[friendsTotals.indexOf(closestMatch)];
+            };
+
+            userTotal = getTotalScore(newFriend.scores);
+            getFriendsListDifference();
+            getFriendMatch();
+            
+            console.log(friendsTotals);
+            console.log(userTotal);
+            console.log(friendMatch);
+            
             friends.push(newFriend);
-            res.json(newFriend);
+
+            //
+            res.send(friendMatch);
         })
     }
 }
